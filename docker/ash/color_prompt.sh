7b1cf698c6475508722f1476457750299d7db0e0
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-05-17 18:54:32 +0900

    feat ash setting

---
+++ docker/ash/color_prompt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+USERNAME=$(whoami)
+if [ "$USERNAME" = root ]; then
+  PS1="[$RED\u@\h $NORMAL\W$RED]# $NORMAL"
+else
+  PS1="[$GREEN\u@\h $NORMAL\W$GREEN]# $NORMAL"
+fi
